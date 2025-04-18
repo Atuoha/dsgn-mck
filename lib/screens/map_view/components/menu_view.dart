17dcfc9ddce91549a2480e4890de1cb266b8be7d
Atuoha Anthony <atuohainitiatives@gmail.com>
2025-04-18 05:06:11 +0100

    menu_view refactor

--- lib/screens/map_view/components/menu_view.dart
+++ lib/screens/map_view/components/menu_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../../../constants/app_color.dart';
 import '../../../model/menu_item.dart';
 
-class MenuView extends StatelessWidget {
+class MenuView extends StatefulWidget {
   const MenuView({
     super.key,
     required this.isMenuShowing,
@@ -17,44 +17,83 @@ class MenuView extends StatelessWidget {
   final Function onItemTapped;
 
   @override
+  State<MenuView> createState() => _MenuViewState();
+}
+
+class _MenuViewState extends State<MenuView> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _heightAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 400),
+      vsync: this,
+    );
+
+    _heightAnimation = Tween<double>(begin: 0.0, end: widget.size.height / 5.5)
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+  }
+
+  @override
+  void didUpdateWidget(covariant MenuView oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.isMenuShowing && !_controller.isAnimating) {
+      _controller.forward();
+    } else if (!widget.isMenuShowing && !_controller.isAnimating) {
+      _controller.reverse();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Positioned(
       left: 30,
       bottom: 160,
-      child: Visibility(
-        visible: isMenuShowing,
-        child: TweenAnimationBuilder<double>(
-          tween: Tween<double>(begin:0.0, end: isMenuShowing ? 1.0 : 0.0),
-            builder: (context, value, child) {
-              double height = value * (size.height / 3.85);
-            return Opacity(
-              opacity: value,
+      child: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) {
+          return _controller.value == 0
+              ? const SizedBox.shrink() // Don't show when fully hidden
+              : Opacity(
+            opacity: _controller.value,
+            child: SizedBox(
+              height: _heightAnimation.value,
+              width: widget.size.width / 2.1,
               child: Container(
-                width: size.width / 2.1,
-                height: height,
                 decoration: BoxDecoration(
-                  color: Colors.white,
+                  color: const Color(0xffFAF5EC),
                   borderRadius: BorderRadius.circular(15),
                 ),
                 child: ListView(
                   padding: EdgeInsets.zero,
+                  physics: const NeverScrollableScrollPhysics(),
                   children: menus.asMap().entries.map((entry) {
                     int index = entry.key;
                     MenuItem menu = entry.value;
                     return GestureDetector(
-                      onTap: () => onItemTapped(index),
+                      onTap: () => widget.onItemTapped(index),
                       child: ListTile(
+                        dense: true,
                         contentPadding: const EdgeInsets.only(left: 10),
+                        visualDensity: const VisualDensity(vertical: -3),
                         leading: Icon(
                           menu.icon,
-                          color: index == selectedMenuIndex
+                          color: index == widget.selectedMenuIndex
                               ? AppColors.primaryColor
                               : AppColors.menuItemInactiveColor,
                         ),
                         title: Text(
                           menu.title,
                           style: TextStyle(
-                            color: index == selectedMenuIndex
+                            color: index == widget.selectedMenuIndex
                                 ? AppColors.primaryColor
                                 : AppColors.menuItemInactiveColor,
                             fontSize: 14,
@@ -65,9 +104,9 @@ class MenuView extends StatelessWidget {
                   }).toList(),
                 ),
               ),
-            );
-          }, duration: const Duration(seconds: 1),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
